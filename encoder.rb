dc449e5fe9b2154de6d2f87f15de6f54047052e9
Atlasi1 <myltykbaev.artur@mail.ru>
2023-09-16 02:19:16 +0300

    added 2 homework (latest files in commit were classwork)

--- encoder.rb
+++ encoder.rb
@@ -1,13 +1,13 @@
 def encoder(str)
   sub_str = ""
   str.downcase!
-  for i in str.split("") do
+  str.split("").each { |i|
     if str.count(i) > 1 or (i in sub_str)
       str = str.sub(i, ")")
       sub_str += i
     else
       str = str.sub(i, "(")
     end
-  end
+  }
   str
 end
